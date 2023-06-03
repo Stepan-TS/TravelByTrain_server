1baf04679bf97a48e722793b84e14752a996fd37
Stepan <tsiolkovskiystepan@gmail.com>
2023-06-03 18:57:56 +0300

    fixed code style

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let trains = [
-  { id: '1', number: '001' },
-  { id: '2', number: '002' },
-];
-
 app.get('/', (req, res) => {
-  res.sendFile('Trains');
+  res.send('Hello!');
 });
 
 app.listen(PORT, () => {
